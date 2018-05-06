e69ff51b51cee9b4dd67dc6daadbc75824849c5b
Loïc P <loic.pirez@epitech.eu>
2018-05-06 12:36:36 +0200

    (it was 9) Lesson 9

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import App from './App'
+import NotFound from './NotFound'
 import StorePicker from './StorePicker'
 
 const Router = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path='/' component={StorePicker} />
+      <Route path='/store/:storeId' component={App} />
+      <Route component={NotFound} />
     </Switch>
   </BrowserRouter>
 )
